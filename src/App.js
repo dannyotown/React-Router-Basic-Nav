f39d135ae75ef3e427f644c53d516f1035ee4d33
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-09-04 19:42:23 -0500

    router name change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
